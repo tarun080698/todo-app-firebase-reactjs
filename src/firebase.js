9fbc0aeb9c7b9392d331bbe0d5557b6489fe3530
tarun080698 <tarun3dadlani@gmail.com>
2020-12-06 17:47:21 +0530

    firebase config restored

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBeEZUalFU957LYBdgyuF-fyPZH6n0o-nY",
-  authDomain: "todolist-react-cp.firebaseapp.com",
-  projectId: "todolist-react-cp",
-  storageBucket: "todolist-react-cp.appspot.com",
-  messagingSenderId: "207279896601",
-  appId: "1:207279896601:web:a3a28ed6c31dd189859288",
-  measurementId: "G-1K5T4R0FJE",
+  
 });
 
 const db = firebaseApp.firestore();
